ee6831c8f6e5b0811f3514434ad2c10b44aa026d
Tyler Anson <tyleranson@Cheech.local>
2016-02-15 21:26:43 -0500

    I added a GUI class and I made some changes to the Player class

---
+++ src/package1/GUI.java
@@ -0,0 +1,7 @@
+package package1;
+
+/**
+ * Created by tyleranson on 2/15/16.
+ */
+public class GUI {
+}
--- src/package1/Player.java
+++ src/package1/Player.java
@@ -1,7 +1,27 @@
 package package1;
 
+import java.awt.*;
+
 /**
  * Created by Charles on 2/15/2016.
  */
 public class Player {
+    protected int x;
+
+    protected int y;
+
+    protected int rotation;
+
+    private int animation;
+
+    public Player() {
+
+    }
+
+    public void draw(Graphics2D g, Game game){
+        g.drawLine(-10, -8, 10, 0);
+        g.drawLine(-10, 8, 10, 0);
+        g.drawLine(-6, -6, -6, 6);
+    }
+
 }
