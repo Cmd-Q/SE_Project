1d97134ce89c23d02cfc0407e63ba8c4adeea5ad
Diego Ruiz <ruizd@mail.gvsu.edu>
2016-02-16 21:32:44 -0500

    added the functionality to move diagonal and so on..

--- src/package1/Player.java
+++ src/package1/Player.java
@@ -15,8 +15,10 @@ import java.awt.geom.Rectangle2D;
 public class Player extends JPanel implements ActionListener,KeyListener {
 
     Timer t = new Timer(5,this);
+    public ImageIcon ship = new ImageIcon("spaceship.png");
+    boolean rightPressed=false, leftPressed=false, downPressed=false, upPressed=false;
 
-    private static int SHIPSIZE = 40;
+    private static int SHIPSIZE = 50;
     private static int BOARDWIDE = 600;
     private static int BOARDHIGH = 600;
     private final static int X_LEFTBOUND = BOARDWIDE-BOARDWIDE;
@@ -25,7 +27,7 @@ public class Player extends JPanel implements ActionListener,KeyListener {
     private final static int Y_DOWNBOUND = BOARDHIGH;
     private static int velocity = 2;
 
-    double x=X_RIGHTBOUND/2,y=Y_DOWNBOUND/2-SHIPSIZE,velX=velocity,velY=velocity;
+    int x=X_RIGHTBOUND/2,y=Y_DOWNBOUND/2-SHIPSIZE,velX=velocity,velY=velocity;
 
     public Player() {
         t.start();
@@ -33,33 +35,18 @@ public class Player extends JPanel implements ActionListener,KeyListener {
         setFocusTraversalKeysEnabled(false);
         setFocusable(true);
     }
-    public void moveUp(){
-        velY = -velocity;
-        velX = 0;
-    }
-    public void moveDown(){
-        velY = velocity;
-        velX = 0;
-    }
-    public void moveLeft(){
-        velX = -velocity;
-        velY = 0;
-    }
-    public void moveRight(){
-        velX = velocity;
-        velY = 0;
-    }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setPaint(Color.BLUE);
-        Rectangle2D box =  new Rectangle2D.Double(x,y,40,40);
-        g2.fill(box);
-        t.start();
-        /*
-        Add what the class will be drawing.
+        /* Displays .png image of a spaceship, this could be used later to move the ship.png around
+         * instead of the drawn blue box
          */
+        Graphics2D g2 = (Graphics2D) g;
+        ship.paintIcon(this, g2, x, y);
+        //Rectangle2D box = new Rectangle2D.Double(x,y,40,40);
+        //g2.setPaint(Color.BLUE);
+        //g2.fill(box);
+        t.start();
     }
 
 //    public void draw(Graphics2D g, Game game){
@@ -91,23 +78,75 @@ public class Player extends JPanel implements ActionListener,KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
-        if(code == KeyEvent.VK_UP){
-            moveUp();
+        if((code == KeyEvent.VK_UP) && (downPressed == false)){
+            velY = -2;
+            upPressed = true;
         }
-        if(code == KeyEvent.VK_DOWN){
-            moveDown();
+        if((code == KeyEvent.VK_UP) && (downPressed == true)){
+            velY = 0;
+            upPressed = true;
         }
-        if(code == KeyEvent.VK_LEFT){
-            moveLeft();
+        if((code == KeyEvent.VK_DOWN) && (upPressed == false)){
+            velY = 2;
+            downPressed = true;
         }
-        if(code == KeyEvent.VK_RIGHT){
-            moveRight();
+        if((code == KeyEvent.VK_DOWN) && (upPressed == true)){
+            velY = 0;
+            downPressed = true;
+        }
+        if((code == KeyEvent.VK_LEFT) && (rightPressed == false)){
+            velX = -2;
+            leftPressed = true;
+        }
+        if((code == KeyEvent.VK_LEFT) && (rightPressed == true)){
+            velX = 0;
+            leftPressed = true;
+        }
+        if((code == KeyEvent.VK_RIGHT) && (leftPressed == false)){
+            velX = 2;
+            rightPressed = true;
+        }
+        if((code == KeyEvent.VK_RIGHT) && (leftPressed == true)) {
+            velX = 0;
+            rightPressed = true;
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        int code = e.getKeyCode();
+        if((code == KeyEvent.VK_UP) && (downPressed == false)){
+            velY = 0;
+            upPressed = false;
+        }
+        if((code == KeyEvent.VK_UP) && (downPressed == true)){
+            velY = 2;
+            upPressed = false;
+        }
+        if((code == KeyEvent.VK_DOWN) && (upPressed == false)){
+            velY = 0;
+            downPressed = false;
+        }
+        if((code == KeyEvent.VK_DOWN) && (upPressed == true)){
+            velY = -2;
+            downPressed = false;
+        }
+        if((code == KeyEvent.VK_LEFT) && (rightPressed == false)){
+            velX = 0;
+            leftPressed = false;
+        }
+        if((code == KeyEvent.VK_LEFT) && (rightPressed == true)){
+            velX = 2;
+            leftPressed = false;
+        }
+        if((code == KeyEvent.VK_RIGHT) && (leftPressed == false)){
+            velX = 0;
+            rightPressed = false;
+        }
+        if((code == KeyEvent.VK_RIGHT) && (leftPressed == true)) {
+            velX = -2;
+            rightPressed = false;
+        }
     }
 
     @Override
