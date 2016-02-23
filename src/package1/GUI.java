32b63a97b9788ce113dc17ea27eb011ce9d652e6
Tyler Anson <tyleranson@Cheech.local>
2016-02-22 20:31:03 -0500

    I updated the GUI class so that the exit button works and we use an action listener instead of a mouse listener

--- src/package1/GUI.java
+++ src/package1/GUI.java
@@ -3,82 +3,64 @@ package package1;
 import javax.swing.*;
 import java.awt.*;
 import javax.swing.JPanel;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by tyleranson on 2/15/16.
  */
-public class GUI extends JPanel implements MouseListener {
-    private JMenu jMenu;
-    private JMenuBar jMenuBar;
+public class GUI extends JPanel implements ActionListener {
+
+    private JMenuBar menuBar;
+
+    private JMenu File;
+
     private JMenuItem restart;
     private JMenuItem exit;
-    protected MouseListener m;
-
-    public static void main(String args[]) {
-        GUI g = new GUI();
-    }
 
     public GUI() {
         Player s = new Player();
         Asteroid a = new Asteroid();
+
         JFrame gameFrame = new JFrame();
+
         setupGUI();
 
-        gameFrame.setJMenuBar(jMenuBar);
-        gameFrame.add(jMenuBar, BorderLayout.NORTH);
+        gameFrame.setJMenuBar(menuBar);
+        gameFrame.add(menuBar, BorderLayout.NORTH);
         gameFrame.add(s);
         //gameFrame.add(a);
         gameFrame.setSize(600, 600);
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameFrame.setVisible(true);
         gameFrame.setTitle("ASTROYED!");
+
     }
+
+    public static void main(String args[]) {
+        GUI g = new GUI();
+    }
+
+
     /*
     Add any GUI options to be built in setupGUI (buttons,menus,othe panels)
      */
     public void setupGUI(){
-        jMenu = new JMenu("Menu");
-        jMenuBar = new JMenuBar();
+        File = new JMenu("File");
+        menuBar = new JMenuBar();
         restart = new JMenuItem("Restart");
         exit = new JMenuItem("Exit");
-        restart.addMouseListener(m);
-        exit.addMouseListener(m);
+        restart.addActionListener(this);
+        exit.addActionListener(this);
 
-        jMenuBar.add(jMenu);
-        jMenu.add(restart);
-        jMenu.add(exit);
+        menuBar.add(File);
+        File.add(restart);
+        File.add(exit);
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        Object userClick = e.getSource();
-        if(userClick == restart){
-            return;
+    public void actionPerformed(ActionEvent e){
+        if(exit == e.getSource()){
+            System.exit(0);
         }
-        if(userClick == exit){
-            return;
-        }
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
     }
 }
